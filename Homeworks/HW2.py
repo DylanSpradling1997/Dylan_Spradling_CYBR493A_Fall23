1fab8e96d3b75c4495c5071cc43bd89ace791955
DylanSpradling1997 <dss0022@mix.wvu.edu>
2023-09-24 18:01:01 -0400

    Created In_Class_4_Pinger.py activity

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,4 +1,33 @@
 """
 Dylan Spradling
 Homework 2
+9/24
+Cyber 493A
+A file that creates the methods used in HW2_main
 """
+
+
+def ValidLength(userinput):
+    if len(userinput) >= 8:
+        return True
+    else:
+        return False
+
+
+def HasNumber(userinput):
+    for x in userinput:
+        if x.isnumeric():
+            return True
+        else:
+            return False
+
+
+def HasSymbol(userinput):
+    specChars = ["!", "@", "#", "$", "%", "*"]
+    for x in userinput:
+        for y in specChars:
+            if x == y:
+                return True
+
+        return False
+
