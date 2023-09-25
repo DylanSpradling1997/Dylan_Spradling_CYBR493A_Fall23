9d020b2d2996ba20b2a2bbfb07c2499d25fdab78
DylanSpradling1997 <dss0022@mix.wvu.edu>
2023-09-25 00:24:37 -0400

    Created methods in HW2

--- In-class activities/In_Class_4_Pinger.py
+++ In-class activities/In_Class_4_Pinger.py
@@ -1,6 +1,13 @@
 import platform
 import os
+"""
+Dylan Spradling 
+in class 4 
+9/24
+493A
+"""
+
 
 PingIDs = {"127.0.0.1","8.0.0.1","192.168.0.10","192.168.10.10"}
-for x in PingIDs:
-        print("")
+for each in PingIDs:
+        print("ping w -1 r -2 "+each+"something something idk")
