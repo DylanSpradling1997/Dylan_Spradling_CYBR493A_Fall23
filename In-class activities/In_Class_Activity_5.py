1330d4d09086d7c3ac6483b3f43b2fbb9bd981e5
DylanSpradling1997 <dss0022@mix.wvu.edu>
2023-09-28 10:17:52 -0400

    Created InClassActivity5

--- In-class activities/In_Class_Activity_5.py
+++ In-class activities/In_Class_Activity_5.py
@@ -1,19 +1,20 @@
 """
+Dylan Spradling
+9/28/2023
 In class activity 5
-
+This program prompts the user for input then calls the desired activity based on the user input
 """
 import IfStatement
 import In_Class_3
 import In_Class_4_Pinger
+
+
 def main():
-    userinput = input("Please select from these option. if-activity: enter 1 In_class_activity3: enter 2  InClassActivity4: press 3")
+    userinput = input(
+        "Please select from these option. if-activity: enter 1 In_class_activity3: enter 2  InClassActivity4: press 3")
     if userinput == 1:
-        IfStatement.number1()
+        return IfStatement
     if userinput == 2:
-        In_Class_3.CoolWordsChecker()
+        return In_Class_3
     if userinput == 3:
-        In_Class_4_Pinger.PingIDs()
-
-
-
-
+        return In_Class_4_Pinger
