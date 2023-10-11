fbe4701a19f184434575171305b8d4b00ee4ee49
DylanSpradling1997 <dss0022@mix.wvu.edu>
2023-10-11 15:32:55 -0400

    chanegd ICA 6 to fix placement error

--- In-class activities/In_Class_Activity6_Ceaser.py
+++ In-class activities/In_Class_Activity6_Ceaser.py
@@ -21,10 +21,10 @@ def main():
     for char in plaintext:
         currentLocation = letters.index(char)
 
-        newLocation = (currentLocation + key)%26
+        newLocation = (currentLocation + key)%25
 
         newChar = letters[newLocation]
-        print(char, currentLocation, newLocation,newChar)
+        print(currentLocation, newLocation, newChar)
 
 if __name__ == "__main__":
     main()
